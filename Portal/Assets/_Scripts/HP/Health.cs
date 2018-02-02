75be0fc44725b65698004c7e15700f1d7d687723
Sponglee <acearutyunyan@gmail.com>
2018-02-02 13:28:46 +0300

    2.02.18
    Re-import of assets for Unity  2017.3.0p4
    health bar slider logic

--- Portal/Assets/_Scripts/HP/Health.cs
+++ Portal/Assets/_Scripts/HP/Health.cs
@@ -1,27 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour {
     public GameObject gameControllerObject;
     public int maxHealth = 100;
     private int minHealth = 1;
     private GameController gc;
+	public Slider healthbar;
+
+
     private void Start()
     {
-        gc = gameControllerObject.GetComponent<GameController>();
-        gc.AddScore(maxHealth);
+		if (gameControllerObject != null)
+		{
+			gc = gameControllerObject.GetComponent<GameController>();
+	        gc.AddScore(maxHealth);
+		}
     }
     public void TakeDamage(int damageAmount)
     {
         if (maxHealth<minHealth)
         {
-            Destroy(this.gameObject);
+			
+			if (this.gameObject.CompareTag("Tower"))
+				{
+					Time.timeScale = 0f;
+				}
+			Destroy(this.gameObject);
             gc.GameOver();
         }
         else
         {
             maxHealth -= damageAmount;
+			healthbar.value -= damageAmount;
             gc.AddScore(maxHealth);
             Debug.Log(maxHealth);
         }
